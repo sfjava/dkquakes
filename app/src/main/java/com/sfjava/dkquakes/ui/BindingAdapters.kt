048d677377d3fa2deb1cff35ee6b4c5314f81bd3
Daniel Kopyc <daniel.kopyc@kroger.com>
2020-10-19 08:35:05 -0700

    add rest of earthquake details to list-item layout

--- app/src/main/java/com/sfjava/dkquakes/ui/BindingAdapters.kt
+++ app/src/main/java/com/sfjava/dkquakes/ui/BindingAdapters.kt
@@ -6,30 +6,27 @@ import androidx.databinding.BindingAdapter
 import java.text.DateFormat
 import java.util.*
 
-class BindingAdapters {
-
-    @BindingAdapter("android:text")
-    fun setText(view: TextView, date: Date?) {
-        if (date != null) {
-            val df: DateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM)
-            val localizedDate: String = df.format(date)
-            view.text = localizedDate
-        }
+@BindingAdapter("android:text")
+fun setText(view: TextView, date: Date?) {
+    if (date != null) {
+        val df: DateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM)
+        val localizedDate: String = df.format(date)
+        view.text = localizedDate
     }
+}
 
-    @BindingAdapter("android:text")
-    fun setText(view: TextView, value: Double?) {
-        if (value != null) {
-            view.text = value.toString()
-        }
+@BindingAdapter("android:text")
+fun setText(view: TextView, value: Double?) {
+    if (value != null) {
+        view.text = value.toString()
     }
+}
 
-    @BindingAdapter("android:date")
-    fun setDate(view: TextView, date: Date?) {
-        if (date != null) {
-            val df: DateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM)
-            val localizedDate: String = df.format(date)
-            view.text = localizedDate
-        }
+@BindingAdapter("android:date")
+fun setDate(view: TextView, date: Date?) {
+    if (date != null) {
+        val df: DateFormat = DateFormat.getDateInstance(DateFormat.MEDIUM)
+        val localizedDate: String = df.format(date)
+        view.text = localizedDate
     }
 }
